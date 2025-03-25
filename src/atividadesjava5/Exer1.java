3789edface2cedeed3b6a5901ffdbf9542b32260
Iago willian <iagowillian22@gmail.com>
2025-03-25 17:27:48 -0300

    Update Exer1.java

--- src/atividadesjava5/Exer1.java
+++ src/atividadesjava5/Exer1.java
@@ -1,15 +1,36 @@
-package atividadesjava5;
+package revisão;
 
-public class Exer1 {
+import java.util.Scanner;
+
+public class Ativ1Condicionais {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      int nA = 2, nB = 4, nC = 5;
-      
-      if (nA + nB >= nC) {
-    	  System.out.println("A soma de A + B é maior que C ");
-		
-	}
+           Scanner lerScanner = new Scanner(System.in);
+           
+           int numeroA, numeroB, numeroC;
+           
+           System.out.println("Digite o primeiro número :");
+           numeroA = lerScanner.nextInt();
+           
+           System.out.println("Digite o segundo número :");
+           numeroB = lerScanner.nextInt();
+           
+           System.out.println("Digite o terceiro número :");
+           numeroC = lerScanner.nextInt();
+           
+           if (numeroA + numeroB > numeroC) {
+        	   System.out.println("a soma de A + B é maior que C ");
+		}
+           if (numeroA + numeroB < numeroC) {
+        	   System.out.println("a soma de A + B é menor que C ");
+		}
+           if (numeroA + numeroB == numeroC) {
+			
+        	   System.out.println("a soma de A + B é igual a C ");
+        	   
+        	   lerScanner.close();
+		}
 	}
 
 }
